82212e8beb1c2cbf228a21c9daf76ae6d9c12e45
Daniel Arandia <dan@the-monastery.com>
2019-06-18 11:32:46 -0700

    Added styling for highlighted words, column headers.

--- frontend/src/common/components/json-reader/json-reader.component.tsx
+++ frontend/src/common/components/json-reader/json-reader.component.tsx
@@ -2,10 +2,6 @@ import * as React from "react";
 
 const style = require("./json-reader.style.scss");
 
-const jsonRenderedWidth = {
-  // width: "1200px"
-};
-
 interface JsonReaderProps {
   className?: string;
   hocr: string;
@@ -22,203 +18,244 @@ export class JsonReaderComponent extends React.PureComponent<JsonReaderProps> {
       <div className={`${style.jsonReaderComponent}`}>
         <div className={`${style.jsonRendered}`}>
           <div className={`${style.viewport}`}>
-            <ul style={jsonRenderedWidth}>
-              <li>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Address</label>
-                  <span>1234 Main St., City, ST 12345</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Phone</label>
-                  <span>000-123-4567</span>
-                </div>
-                <div
-                  className={`${style.lineItemDetail} ${style.hasChildren} ${
-                    style.selected
-                  }`}
-                >
-                  <label>Employees (5)</label>
-                </div>
-                <div className={`${style.lineItemDetail} ${style.hasChildren}`}>
-                  <label>Contractors (12)</label>
-                </div>
-              </li>
-            </ul>
+            <div className={`${style.column}`}>
+              <div className={`${style.columnHeader}`}>
+                <label>Top</label>
+              </div>
+              <ul>
+                <li>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Address</label>
+                    <span>1234 Main St., City, ST 12345</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Phone</label>
+                    <span>000-123-4567</span>
+                  </div>
+                  <div
+                    className={`${style.lineItemDetail} ${style.hasChildren} ${
+                      style.selected
+                    }`}
+                  >
+                    <label>Employees (5)</label>
+                  </div>
+                  <div
+                    className={`${style.lineItemDetail} ${style.hasChildren} ${
+                      style.hasSearchItem
+                    }`}
+                  >
+                    <label>Contractors (12)</label>
+                  </div>
+                </li>
+              </ul>
+            </div>
 
-            <ul>
-              <li>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>First Name</label>
-                  <span>Joseph</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Last Name</label>
-                  <span>Johnson</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Position</label>
-                  <span>Owner, CEO</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Biography</label>
-                  <span>
-                    Lorem Ipsum has been the company's standard dummy text ever
-                    since the 1500s, when an unknown printer took a galley of
-                    type and scrambled it to make a type specimen book.
-                  </span>
-                </div>
-                <div
-                  className={`${style.lineItemDetail} ${style.hasChildren} ${
-                    style.selected
-                  }`}
-                >
-                  <label>Contact Info (3)</label>
-                </div>
-              </li>
+            <div className={`${style.column}`}>
+              <div className={`${style.columnHeader}`}>
+                <label>Employees</label>
+              </div>
+              <ul>
+                <li>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>First Name</label>
+                    <span>Joseph</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Last Name</label>
+                    <span>Johnson</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Position</label>
+                    <span>Owner, CEO</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Biography</label>
+                    <span>
+                      Lorem Ipsum has been the <em>company</em>'s standard dummy
+                      text ever since the 1500s, when an unknown printer took a
+                      galley of type and scrambled it to make a type specimen
+                      book.
+                    </span>
+                  </div>
+                  <div
+                    className={`${style.lineItemDetail} ${style.hasChildren} ${
+                      style.selected
+                    }`}
+                  >
+                    <label>Contact Info (3)</label>
+                  </div>
+                </li>
 
-              <li>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>First Name</label>
-                  <span>Molly</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Last Name</label>
-                  <span>Reynolds</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Position</label>
-                  <span>CFO</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Biography</label>
-                  <span>
-                    Lorem Ipsum has been the company's standard dummy text ever
-                    since the 1500s, when an unknown printer took a galley of
-                    type and scrambled it to make a type specimen book.
-                  </span>
-                </div>
-                <div className={`${style.lineItemDetail} ${style.hasChildren}`}>
-                  <label>Contact Info (3)</label>
-                </div>
-              </li>
+                <li>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>First Name</label>
+                    <span>Molly</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Last Name</label>
+                    <span>Reynolds</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Position</label>
+                    <span>CFO</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Biography</label>
+                    <span>
+                      Lorem Ipsum has been the <em>company</em>'s standard dummy
+                      text ever since the 1500s, when an unknown printer took a
+                      galley of type and scrambled it to make a type specimen
+                      book.
+                    </span>
+                  </div>
+                  <div
+                    className={`${style.lineItemDetail} ${style.hasChildren}`}
+                  >
+                    <label>Contact Info (3)</label>
+                  </div>
+                </li>
 
-              <li>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>First Name</label>
-                  <span>Steve</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Last Name</label>
-                  <span>Sanders</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Position</label>
-                  <span />
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Biography</label>
-                  <span>
-                    Aliquam aliquam turpis id elit egestas, id malesuada tellus
-                    vestibulum. Proin nibh urna, efficitur eu
-                    auctorpellentesque.
-                  </span>
-                </div>
-                <div className={`${style.lineItemDetail} ${style.hasChildren}`}>
-                  <label>Contact Info (3)</label>
-                </div>
-              </li>
+                <li>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>First Name</label>
+                    <span>Steve</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Last Name</label>
+                    <span>Sanders</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Position</label>
+                    <span />
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Biography</label>
+                    <span>
+                      Aliquam aliquam turpis id elit egestas, id malesuada
+                      tellus vestibulum. Proin nibh urna, efficitur eu
+                      auctorpellentesque.
+                    </span>
+                  </div>
+                  <div
+                    className={`${style.lineItemDetail} ${style.hasChildren}`}
+                  >
+                    <label>Contact Info (3)</label>
+                  </div>
+                </li>
 
-              <li>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>First Name</label>
-                  <span>Tina</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Last Name</label>
-                  <span>Tallworth</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Position</label>
-                  <span>Company Manager</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Biography</label>
-                  <span>
-                    Aliquam aliquam turpis id elit egestas, id malesuada tellus
-                    vestibulum. Proin nibh urna, efficitur eu
-                    auctorpellentesque.
-                  </span>
-                </div>
-                <div className={`${style.lineItemDetail} ${style.hasChildren}`}>
-                  <label>Contact Info (3)</label>
-                </div>
-              </li>
-            </ul>
+                <li>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>First Name</label>
+                    <span>Tina</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Last Name</label>
+                    <span>Tallworth</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Position</label>
+                    <span>
+                      <em>Company</em> Manager
+                    </span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Biography</label>
+                    <span>
+                      Aliquam aliquam turpis id elit egestas, id malesuada
+                      tellus vestibulum. Proin nibh urna, efficitur eu
+                      auctorpellentesque.
+                    </span>
+                  </div>
+                  <div
+                    className={`${style.lineItemDetail} ${style.hasChildren}`}
+                  >
+                    <label>Contact Info (3)</label>
+                  </div>
+                </li>
+              </ul>
+            </div>
 
-            <ul>
-              <li>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Phone</label>
-                  <span>111-111-1111</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Email</label>
-                  <span>j-johnson@abc-company.com</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Twitter</label>
-                  <span>@jjohnson</span>
-                </div>
-                <div
-                  className={`${style.lineItemDetail} ${style.hasChildren} ${
-                    style.selected
-                  }`}
-                >
-                  <label>Extra Layer (2)</label>
-                </div>
-              </li>
-            </ul>
+            <div className={`${style.column}`}>
+              <div className={`${style.columnHeader}`}>
+                <label>Contact Info</label>
+              </div>
+              <ul>
+                <li>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Phone</label>
+                    <span>111-111-1111</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Email</label>
+                    <span>
+                      j-johnson@abc-<em>company</em>.com
+                    </span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Twitter</label>
+                    <span>@jjohnson</span>
+                  </div>
+                  <div
+                    className={`${style.lineItemDetail} ${style.hasChildren} ${
+                      style.selected
+                    }`}
+                  >
+                    <label>Extra Layer (2)</label>
+                  </div>
+                </li>
+              </ul>
+            </div>
 
-            <ul>
-              <li>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Key</label>
-                  <span>Value</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Key 2</label>
-                  <span>
-                    Lorem ipsum dolor sit amet, consect etur adipiscing elit.
-                  </span>
-                </div>
-                <div
-                  className={`${style.lineItemDetail} ${style.hasChildren} ${
-                    style.selected
-                  }`}
-                >
-                  <label>Extra, Extra Layer (2)</label>
-                </div>
-              </li>
-            </ul>
+            <div className={`${style.column}`}>
+              <div className={`${style.columnHeader}`}>
+                <label>Extra Layer</label>
+              </div>
+              <ul>
+                <li>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Key</label>
+                    <span>Value</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Key 2</label>
+                    <span>
+                      Lorem ipsum dolor sit amet, consect etur adipiscing elit.
+                    </span>
+                  </div>
+                  <div
+                    className={`${style.lineItemDetail} ${style.hasChildren} ${
+                      style.selected
+                    }`}
+                  >
+                    <label>Extra, Extra Layer (2)</label>
+                  </div>
+                </li>
+              </ul>
+            </div>
 
-            <ul>
-              <li>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Key</label>
-                  <span>Value</span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Key 2</label>
-                  <span>
-                    Lorem ipsum dolor sit amet, consect etur adipiscing elit.
-                  </span>
-                </div>
-                <div className={`${style.lineItemDetail}`}>
-                  <label>Key 3</label>
-                  <span>Value</span>
-                </div>
-              </li>
-            </ul>
+            <div className={`${style.column}`}>
+              <div className={`${style.columnHeader}`}>
+                <label>Extra, Extra Layer</label>
+              </div>
+              <ul>
+                <li>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Key</label>
+                    <span>Value</span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Key 2</label>
+                    <span>
+                      Lorem ipsum dolor sit amet, consect etur adipiscing elit.
+                    </span>
+                  </div>
+                  <div className={`${style.lineItemDetail}`}>
+                    <label>Key 3</label>
+                    <span>Value</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
